3776eb95c2ad38c6a975c4d2f02e8c58a72dc110
yunmap <kalmpink@korea.ac.kr>
2018-02-10 17:59:02 +0900

    block chain seminar

---
+++ truffle/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "blkchnmyfrah.eastus.cloudapp.azure.com:8545",
+            port: 8545,
+            gas: 4995119,
+            network_id: "*" // Match any network id
+        }
+    }
+};
